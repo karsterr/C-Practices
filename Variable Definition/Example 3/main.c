b82c6224842a19fe964e6fdb3ce293331f5eaba8
Efe Can Kara <efecan_kara@proton.me>
2023-11-29 13:23:15 -0800

    Write the code of Variable Definition - 3

---
+++ Variable Definition/Example 3/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <stdio.h>
+
+int main() {
+    int num;
+    double costBook = 25.50;
+    char device[30] = "Intel";
+
+    // int sayi = 52; // Error
+    // Fix of this error:
+    num = 52;
+
+    printf("%d", num);
+    return 0;
+}
