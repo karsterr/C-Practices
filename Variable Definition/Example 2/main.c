0145d55680b08d817d8330f4746556d9c5ed6b04
Efe Can Kara <efecan_kara@proton.me>
2023-11-29 13:20:21 -0800

    Write the code of Variable Definition - 2

---
+++ Variable Definition/Example 2/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+
+    int number = 4;
+
+    int p;
+    p = 10;
+
+    float pi;
+    pi = 3.14;
+
+    int value;
+    printf("\nNew value > ");
+    scanf("%d", &value);
+    printf("\nNew value is > %d", value);
+
+
+    double k, m = 3.0, x, y = 4.0, z;
+    printf("\n\nVariable Values\nk=%e\nm=%e\nx=%e\ny=%e\nz=%e\n", k, m, x, y, z);
+
+    int a, b, c;
+    a = b = c = 5;
+    printf("\na=%d,b=%d,c=%d\n", a, b, c);
+    return 0;
+}
+
+
