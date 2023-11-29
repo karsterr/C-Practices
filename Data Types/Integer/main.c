b8cc31717ff6cbdc89cf0dc387f9815da2d406d9
Efe Can Kara <efecan_kara@proton.me>
2023-11-29 13:27:41 -0800

    Write the code of Data Types - 1

---
+++ Data Types/Integer/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int intVar = 756;
+    printf("Integer variable value: %d\n", intVar);
+
+    printf("Integer variable value: %i\n", intVar);
+
+    intVar = -100;
+    printf("Integer variable value: %d\n", intVar);
+
+    return 0;
+}
