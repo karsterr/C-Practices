3ba97fc16f680633996d2aa7f8b7781f91e3bfee
Efe Can Kara <efecan_kara@proton.me>
2023-11-29 05:42:34 -0800

    Write the code of ascii.c

---
+++ ASCII Values of Certain Typed Values/ascii.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    char a = 'A';
+    char b = 67;
+    char c = 97;
+
+    printf("Character Value of a : %c\n", a);
+    printf("ASCII Value of a : %d\n\n", a);
+
+    printf("Character Value of b : %c\n", b);
+    printf("ASCII Value of b : %d\n\n", b);
+
+    printf("Character Value of c : %c\n", c);
+    printf("ASCII Value of c : %d", c);
+
+    return 0;
+}
