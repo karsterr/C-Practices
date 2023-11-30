91ed2150ba7469183fa67dfa9cd24001896a6ccc
Efe Can Kara <efecan_kara@proton.me>
2023-11-30 03:06:18 -0800

    Write the code of logic.c

--- Operators/Logic/logic.c
+++ Operators/Logic/logic.c
@@ -1,45 +1,27 @@
-/* 
- * File:   main.c
- * Author: tkiziloren
- *
- * Created on 25 Ağustos 2012 Cumartesi, 20:41
- */
-
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    //Mantıksal operatör kullanım örnekleri
+int main() {
     int a = 1, b = 2, c = 3;
-    // && kullanımı örnek
+
     if ((a < b) && (a < c))
-        printf("\n%d degeri %d ve %d'den kucuktur.", a, b, c);
+        printf("\n%d is less than %d and %d.", a, b, c);
 
-    // || kullanımı örnek
     if ((a < b) || (a < c))
-        printf("\n%d, %d'den kucuk veya %d, %d'den kucuktur.", a, b, a, c);
+        printf("\n%d is less than %d or %d is less than %d.", a, b, a, c);
 
-    // ! kullanımı örnek
-    if (!(a < b))
-        printf("\n%d degeri %d'den buyuk veya esittir.", a, b);
-
-    // a ve b değişkenlerinin değerlerini eşitleyelim
     a = 5, b = 5;
-    // ! kullanımı örnek
+
     if (!(a < b))
-        printf("\n%d degeri %d'den buyuk veya esittir.", a, b);
+        printf("\n%d is equal or greater than %d.", a, b);
 
-    // xor kullanımı örnek
     if (a ^ b)
-        printf("\n%d degeri %d'den farklidir.", a, b);
+        printf("\n%d and %d are different values.", a, b);
 
-    // farklinin degili esit anlamina gelir
     if (!(a ^ b))
-        printf("\n%d degeri %d degerine esittir.", a, b);
+        printf("\n%d and %d are equal", a, b);
 
     if (a ^ c)
-        printf("\n%d degeri %d'den farklidir.", a, c);
-
-
+        printf("\n%d and %d are different values.", a, c);
 
     return 0;
 }
