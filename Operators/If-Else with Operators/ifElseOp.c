638f61ccc4e25f8b190c67c1a838bbf785d782a9
Efe Can Kara <efecan_kara@proton.me>
2023-11-30 02:56:56 -0800

    Write the code of ifElseOp.c

--- Operators/If-Else with Operators/ifElseOp.c
+++ Operators/If-Else with Operators/ifElseOp.c
@@ -1,27 +1,12 @@
-/* 
- * File:   main.c
- * Author: tkiziloren
- *
- * Created on 25 Ağustos 2012 Cumartesi, 21:06
- */
-
 #include <stdio.h>
 
-int main(int argc, char** argv) {
+int main() {
+    int x = 11, y = 5, p = 0, q = 0;
+    p = x > y ? x : y;
+    q = x < y ? x : y;
 
-    int x = 11, y = 5, z = 0, z1 = 0;
-
-    /* x degeri y'den buyukse z'nin değeri x olsun
-       aksi takdirde y olsun */
-    z = x > y ? x : y;
-    
-    /* x degeri y'den kucukse z'nin değeri x olsun
-       aksi takdirde y olsun */   
-    z1 = x < y ? x : y;
-
-    printf("z  : %d\n", z);
-    printf("z1 : %d\n", z1); 
-
+    printf("z : %d\n", p);
+    printf("q : %d\n", q);
     return 0;
 }
 
