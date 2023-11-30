7c84701768e074956a8b1f63e337c03d25bfb909
Efe Can Kara <efecan_kara@proton.me>
2023-11-30 03:14:41 -0800

    Write the code of prePost.c

--- Operators/Pre and Post Increases/prePost.c
+++ Operators/Pre and Post Increases/prePost.c
@@ -1,37 +1,22 @@
-/* 
- * File:   main.c
- * Author: tkiziloren
- *
- * Created on 25 Ağustos 2012 Cumartesi, 20:31
- */
+#include <stdio.h>
 
-#include <stdio.h> 
+int main() {
+    int x = 5;
+    int pre = ++x;
+    int post = x++;
 
-int main(int argc, const char * argv[]) {
-    int sayi1 = 5;
-    int sayi2 = 5;
-    // Önce bir artır, sonra atama yap
-    int iOnceArtirSonuc = ++sayi1;
-    // Önce atama yap, sonra bir artır
-    int iSonraArtirSonuc = sayi2++;
-
-    printf("sayi1=%d\n", sayi1);
-    printf("sayi2=%d\n", sayi2);
-
-    printf("iOnceArtirSonuc=%d\n", iOnceArtirSonuc);
-    printf("iSonraArtirSonuc=%d\n", iSonraArtirSonuc);
+    printf("x = %d\n", x);
+    printf("Pre Increasement = %d\n", pre);
+    printf("Post Increasement = %d\n", post);
 
     printf("------------------------\n");
 
-    //Önce bir eksilt, sonra atama yap
-    int iOnceEksiltSonuc = --sayi1;
-    //Önce atama yap, sonra bir eksilt 
-    int iSonraEksiltSonuc = sayi2--;
+    x = 5;
+    pre  = --x;
+    post = x--;
 
-    printf("sayi1=%d\n", sayi1);
-    printf("sayi2=%d\n", sayi2);
-
-    printf("iOnceEksiltSonuc=%d\n", iOnceEksiltSonuc);
-    printf("iSonraEksiltSonuc=%d\n", iSonraEksiltSonuc);
+    printf("x = %d\n", x);
+    printf("Pre Decreasement = %d\n", pre);
+    printf("Post Increasement = %d\n", post);
     return 0;
 }
